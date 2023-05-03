b77d547479cca779a9ef0e100a703c718cf0b9a2
AnaghaWani <115250606+AnaghaWani@users.noreply.github.com>
2023-05-04 01:35:03 +0530

    Add files via upload

--- lib/screens/app_style.dart
+++ lib/screens/app_style.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 const Color kBlue = Color(0xff0081A8);
 const Color kGreen = Color(0xff01AFBA);
-const Color kYellow = Color(0xffFDFCDD);
-const Color kOrange = Color(0xffFDD9B7);
-const Color kRed = Color(0xffF07168);
+const Color kYellow = Color(0xffEEEEEE);
+// const Color kOrange = Color(0xffFDD9B7);
+const Color kRed = Color(0xffFF2E63);
+const Color kGrey = Color(0xff393D46);
+const Color kBlack = Color(0xff212832);
+
 const double kPaddingHorizontal = 30.0;
 
 final kSourceSansProBold = GoogleFonts.sourceSansPro(
-  fontWeight: FontWeight.w700,
+  fontWeight: FontWeight.w700, color:kBlack ,
 );
 final kSourceSansProSemiBold = GoogleFonts.sourceSansPro(
-  fontWeight: FontWeight.w600,
+  fontWeight: FontWeight.w600, color:kBlack ,
 );
 final kSourceSansProMedium = GoogleFonts.sourceSansPro(
-  fontWeight: FontWeight.w500,
+  fontWeight: FontWeight.w500, color:kBlack ,
 );
 final kSourceSansProRegular = GoogleFonts.sourceSansPro(
-  fontWeight: FontWeight.w400,
+  fontWeight: FontWeight.w400, color:kBlack ,
 );
